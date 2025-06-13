16296fdf8290f14f684471e1137f5de97443e9c8
Prince stanley Jean baptiste <prstanley@Princes-MacBook-Air.local>
2025-06-13 14:32:13 -0400

    initialize project

---
+++ app/fake_db.py
@@ -0,0 +1,14 @@
+def get_data():
+    return {
+        1: {
+            "nom": "Prince",
+            "prenom": "Stanley"
+        },
+        2: {
+            "nom": "Jean Baptiste",
+            "prenom": "Prince"
+        }
+    }
+
+
+print(get_data()[1])
